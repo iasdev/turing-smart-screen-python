8d2d24af435ae3ad1e58d5bd4b94630d7c330ec1
IAS <iarjona1995@gmail.com>
2025-06-03 15:03:08 +0200

    feat: custom sensors

--- library/sensors/sensors_custom.py
+++ library/sensors/sensors_custom.py
@@ -1,98 +1,143 @@
-# turing-smart-screen-python - a Python system monitor and library for USB-C displays like Turing Smart Screen or XuanFang
-# https://github.com/mathoudebine/turing-smart-screen-python/
-# Copyright (C) 2021-2023  Matthieu Houdebine (mathoudebine)
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-# This file allows to add custom data source as sensors and display them in System Monitor themes
-# There is no limitation on how much custom data source classes can be added to this file
-# See CustomDataExample theme for the theme implementation part
-
+import os
 import math
 import platform
+import clr
+import sys
+import time
+from pathlib import Path
 from abc import ABC, abstractmethod
 from typing import List
 
+dll_path = os.path.join(os.path.dirname(__file__), "../../external/LibreHardwareMonitor/LibreHardwareMonitorLib.dll")
+clr.AddReference(dll_path)
+from LibreHardwareMonitor.Hardware import Computer, HardwareType, SensorType
 
-# Custom data classes must be implemented in this file, inherit the CustomDataSource and implement its 2 methods
+computer = Computer()
+computer.IsGpuEnabled = True
+computer.IsCpuEnabled = True
+computer.Open()
+
+def update_hardware(hardware):
+    hardware.Update()
+    for subhardware in hardware.SubHardware:
+        subhardware.Update()
+
+# Funciones para obtener valores (adaptadas de las que ya tenías)
+def get_gpu_vram():
+    for hardware in computer.Hardware:
+        if hardware.HardwareType.ToString() in ["GpuNvidia", "GpuAmd", "GpuIntel"]:
+            hardware.Update()
+            for sensor in hardware.Sensors:
+                if sensor.SensorType == SensorType.Temperature and sensor.Name == "GPU Memory":
+                    return sensor.Value
+    return None
+
+def get_gpu_hotspot():
+    for hardware in computer.Hardware:
+        if hardware.HardwareType.ToString() in ["GpuNvidia", "GpuAmd", "GpuIntel"]:
+            hardware.Update()
+            for sensor in hardware.Sensors:
+                if sensor.SensorType == SensorType.Temperature and sensor.Name == "GPU Hot Spot":
+                    return sensor.Value
+    return None
+
+def get_gpu_power_watts():
+    for hardware in computer.Hardware:
+        if hardware.HardwareType.ToString() in ["GpuNvidia", "GpuAmd", "GpuIntel"]:
+            hardware.Update()
+            for sensor in hardware.Sensors:
+                if sensor.SensorType == SensorType.Power and sensor.Name == "GPU Package":
+                    return sensor.Value
+    return None
+
+def get_cpu_power_watts():
+    for hardware in computer.Hardware:
+        if hardware.HardwareType == HardwareType.Cpu:
+            hardware.Update()
+            for sensor in hardware.Sensors:
+                # Aquí el sensor puede llamarse "Package" o parecido
+                if sensor.SensorType == SensorType.Power and "package" in sensor.Name.lower():
+                    return sensor.Value
+    return None
+
 class CustomDataSource(ABC):
     @abstractmethod
     def as_numeric(self) -> float:
-        # Numeric value will be used for graph and radial progress bars
-        # If there is no numeric value, keep this function empty
         pass
 
     @abstractmethod
     def as_string(self) -> str:
-        # Text value will be used for text display and radial progress bar inner text
-        # Numeric value can be formatted here to be displayed as expected
-        # It is also possible to return a text unrelated to the numeric value
-        # If this function is empty, the numeric value will be used as string without formatting
         pass
 
     @abstractmethod
     def last_values(self) -> List[float]:
-        # List of last numeric values will be used for plot graph
-        # If you do not want to draw a line graph or if your custom data has no numeric values, keep this function empty
         pass
 
-
-# Example for a custom data class that has numeric and text values
-class ExampleCustomNumericData(CustomDataSource):
-    # This list is used to store the last 10 values to display a line graph
-    last_val = [math.nan] * 10  # By default, it is filed with math.nan values to indicate there is no data stored
+class CPUWatts(CustomDataSource):
+    _history: List[float] = []
 
     def as_numeric(self) -> float:
-        # Numeric value will be used for graph and radial progress bars
-        # Here a Python function from another module can be called to get data
-        # Example: self.value = my_module.get_rgb_led_brightness() / audio.system_volume() ...
-        self.value = 75.845
-
-        # Store the value to the history list that will be used for line graph
-        self.last_val.append(self.value)
-        # Also remove the oldest value from history list
-        self.last_val.pop(0)
-
-        return self.value
+        value = get_cpu_power_watts()
+        if value is not None and math.isfinite(value):
+            self._history.append(value)
+            return value
+        return 0.0
 
     def as_string(self) -> str:
-        # Text value will be used for text display and radial progress bar inner text.
-        # Numeric value can be formatted here to be displayed as expected
-        # It is also possible to return a text unrelated to the numeric value
-        # If this function is empty, the numeric value will be used as string without formatting
-        # Example here: format numeric value: add unit as a suffix, and keep 1 digit decimal precision
-        return f'{self.value:>5.1f}%'
-        # Important note! If your numeric value can vary in size, be sure to display it with a default size.
-        # E.g. if your value can range from 0 to 9999, you need to display it with at least 4 characters every time.
-        # --> return f'{self.as_numeric():>4}%'
-        # Otherwise, part of the previous value can stay displayed ("ghosting") after a refresh
+        val = self.as_numeric()
+        # truncamos la parte decimal
+        return f"{int(val)} W" if val > 0 else "0 W"
 
     def last_values(self) -> List[float]:
-        # List of last numeric values will be used for plot graph
-        return self.last_val
+        return self._history[-10:]
 
+class GPUWatts(CustomDataSource):
+    _history: List[float] = []
 
-# Example for a custom data class that only has text values
-class ExampleCustomTextOnlyData(CustomDataSource):
     def as_numeric(self) -> float:
-        # If there is no numeric value, keep this function empty
-        pass
+        value = get_gpu_power_watts()
+        if value is not None and math.isfinite(value):
+            self._history.append(value)
+            return value
+        return 0.0
 
     def as_string(self) -> str:
-        # If a custom data class only has text values, it won't be possible to display graph or radial bars
-        return "Python: " + platform.python_version()
+        val = self.as_numeric()
+        return f"{int(val)} W" if val > 0 else "0 W"
 
     def last_values(self) -> List[float]:
-        # If a custom data class only has text values, it won't be possible to display line graph
-        pass
+        return self._history[-10:]
+
+class GPUHotspotTemp(CustomDataSource):
+    _history: List[float] = []
+
+    def as_numeric(self) -> float:
+        value = get_gpu_hotspot()
+        if value is not None and math.isfinite(value):
+            self._history.append(value)
+            return value
+        return 0.0
+
+    def as_string(self) -> str:
+        val = self.as_numeric()
+        return f"{int(val)}ºC" if val > 0 else "0ºC"
+
+    def last_values(self) -> List[float]:
+        return self._history[-10:]
+
+class GPUVRAMTemp(CustomDataSource):
+    _history: List[float] = []
+
+    def as_numeric(self) -> float:
+        value = get_gpu_vram()
+        if value is not None and math.isfinite(value):
+            self._history.append(value)
+            return value
+        return 0.0
+
+    def as_string(self) -> str:
+        val = self.as_numeric()
+        return f"{int(val)}ºC" if val > 0 else "0ºC"
+
+    def last_values(self) -> List[float]:
+        return self._history[-10:]
